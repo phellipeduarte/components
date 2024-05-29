21809fc287752c6e70c4c0e4166e35e81828ca9a
phellipeduarte <phellipe.duarte@icloud.com>
2024-05-29 11:40:30 -0300

    Criação do componente Button

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,20 @@
+interface ButtonProps {
+    text: string;
+    link: string;
+    type: Type;
+}
+
+export enum Type {
+    Main = "bg-sky-400",
+    Sec = "bg-amber-600",
+}
+
+export const Button = (props: ButtonProps) => {
+    return (
+        <div className="mt-5">
+            <a className={"text-lg text-center font-bold text-slate-50 px-16 py-3 rounded-md max-w-80 uppercase no-underline " + props.type} href={props.link}>
+                {props.text}
+            </a>
+        </div>
+    )
+}
